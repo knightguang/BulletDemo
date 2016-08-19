a66add26cb8d03fa7f1fc8bf0f7c768d2c9b2de3
guang <331689079@qq.com>
2016-08-19 15:46:45 +0800

    Initial Commit

---
+++ BulletDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BulletDemo
+//
+//  Created by 光 on 16/8/19.
+//  Copyright © 2016年 光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BulletDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BulletDemo
+//
+//  Created by 光 on 16/8/19.
+//  Copyright © 2016年 光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
